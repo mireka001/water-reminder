7ab206d57c012d587bdb321a40f15677c8bbb0a1
morgy222 <94820757+morgy222@users.noreply.github.com>
2021-12-21 22:13:06 -0500

    Update and rename discord.py to main.py

---
+++ bot/main.py
@@ -0,0 +1,37 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('hi hi!')
+
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('i love you'):
+        await message.channel.send('bitch, im a bot :(')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('pls hug'):
+        await message.channel.send('https://media.giphy.com/media/3M4NpbLCTxBqU/giphy.gif')
+    
+     
+    if message.content.startswith('pls kissy'):
+        await message.channel.send('https://media.giphy.com/media/iJDthUh1cvaF2/giphy.gif')
+   
+    if message.content.startswith('pls codenames'):
+        await message.channel.send('https://codenames.game/')  
+client.run(os.getenv("DISCORD_TOKEN"))
