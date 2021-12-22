150166bf51f398bf1c105a826073937bde8524a1
morgy222 <94820757+morgy222@users.noreply.github.com>
2021-12-21 21:37:09 -0500

    Update discord.py

--- bot/discord.py
+++ bot/discord.py
@@ -1,16 +1,88 @@
+import discord
 import os
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print('We have logged in as {0.user}'.format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+    if message.content.startswith('hello'):
+        await message.channel.send('hi hi!')
+
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('i love you'):
+        await message.channel.send('bitch, im a bot :(')
+
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('pls hug'):
+        await message.channel.send('https://media.giphy.com/media/3M4NpbLCTxBqU/giphy.gif')
+    
+     
+    if message.content.startswith('pls kissy'):
+        await message.channel.send('https://media.giphy.com/media/iJDthUh1cvaF2/giphy.gif')
+   
+    if message.content.startswith('pls codenames'):
+        await message.channel.send('https://codenames.game/')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+    
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')  
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+    
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')
+
+    if message.content.startswith(''):
+        await message.channel.send('')           
+client.run(('OTIyNzE5MjY0MzQxODk3Mjg3.YcFjRw.FMxK2nQ2G-uhrXWC_b7RKRff1Ds'))
