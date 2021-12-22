9356ade13e44ae82ccb0ae8ec4762c78b3122fed
morgy222 <94820757+morgy222@users.noreply.github.com>
2021-12-21 22:58:42 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -12,63 +12,60 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.startswith('.m hello'):
         await message.channel.send('hi hi!')
 
 
     if message.author == client.user:
         return
 
-    if message.content.startswith('i love you'):
+    if message.content.startswith('.m i love you'):
         await message.channel.send('bitch, im a bot :(')
 
     if message.author == client.user:
         return
 
-    if message.content.startswith('pls hug'):
+    if message.content.startswith('.m hug'):
         await message.channel.send('https://media.giphy.com/media/3M4NpbLCTxBqU/giphy.gif')
     
      
-    if message.content.startswith('pls kissy'):
+    if message.content.startswith('.m kissy'):
         await message.channel.send('https://media.giphy.com/media/iJDthUh1cvaF2/giphy.gif')
    
-    if message.content.startswith('pls codenames'):
+    if message.content.startswith('.m codenames'):
         await message.channel.send('https://codenames.game/')  
 
-    if message.content.startswith('pls gartic phone'):
+    if message.content.startswith('.m gartic phone'):
         await message.channel.send('https://garticphone.com/') 
 
-    if message.content.startswith('pls secret hitler'):
+    if message.content.startswith('.m secret hitler'):
         await message.channel.send('https://secrethitler.io/')
 
-    if message.content.startswith('pls uno'):
+    if message.content.startswith('.m uno'):
         await message.channel.send('https://www.letsplayuno.com/')           
-    if message.content.startswith('pls scrabble'):
+    if message.content.startswith('.m scrabble'):
         await message.channel.send('https://www.pogo.com/games/scrabble/play') 
 
-    if message.content.startswith('pls skribbl'):
+    if message.content.startswith('.m skribbl'):
         await message.channel.send('https://skribbl.io/')  
     
-    if message.content.startswith('pls heart'):
+    if message.content.startswith('.m heart'):
         await message.channel.send('https://tenor.com/view/moti-hearts-gif-20367288') 
         
-    if message.content.startswith('pls narwhale'):
+    if message.content.startswith('.m narwhale'):
         await message.channel.send('http://narwhale.io/')
        
     
-    if message.content.startswith('pls somebody love me, for fucks sake why does nobody love me!'):
-        await message.channel.send('sorry meme :( we all love you here!!! <3')     
+    if message.content.startswith('.m somebody love me, for fucks sake why does nobody love me!'):
+        await message.channel.send('sorry meme :( we all love you here!!! <3') 
         
-    if message.content.startswith('Haiku will love you no worries meme'):
-        await message.channel.send('NO! beep boop beep boop :( <3')
+    if message.content.startswith('.m meme'):
+        await message.channel.send('we all love you here meme!!! <3')     
         
-    if message.content.startswith('pls somebody love me, for fucks sake why does nobody love me!'):
-        await message.channel.send('sorry meme :( we all love you here!!! <3')     
-        
-    if message.content.startswith('pls wink'):
+    if message.content.startswith('.m wink'):
         await message.channel.send('https://tenor.com/bNyLU.gif') 
         
-    if message.content.startswith('pls youtube'):
+    if message.content.startswith('.m youtube'):
        await message.channel.send('https://www.youtube.com/')
     
 client.run(os.getenv("DISCORD_TOKEN"))
