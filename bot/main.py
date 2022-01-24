1e333c48006d327010e4f28a812dd32b7016eb4b
morgy222 <94820757+morgy222@users.noreply.github.com>
2022-01-24 04:35:31 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -13,7 +13,7 @@ async def on_message(message):
         return
 
     if message.content.lower().strip().startswith('.m hello'):
-        await message.channel.send('hi hi! https://media4.giphy.com/media/lrtPCNjE8Rdoky43VR/200.gif')
+        await message.channel.send('hi hi! https://c.tenor.com/4pSNX9roPqEAAAAd/menitrust-norton-commander.gif')
 
 
     if message.author == client.user:
@@ -42,35 +42,47 @@ async def on_message(message):
    
     if message.content.lower().strip().startswith('.m  somebody love me, for fucks sake why does nobody love me!'):
         await message.channel.send('sorry <@324054566595198976> :( we all love you here!!! <3') 
+
         
     if message.content.lower().strip().startswith('.m meme'):
-        await message.channel.send('we all love you here <@324054566595198976>!!! <3')     
+        await message.channel.send('we all love you here <@324054566595198976>!!! <3')  
+   
         
     if message.content.lower().strip().startswith('.m wink'):
         await message.channel.send('https://tenor.com/bNyLU.gif') 
+
         
     if message.content.lower().strip().startswith('.m youtube'):
        await message.channel.send('https://www.youtube.com/')
 
+
     if message.content.lower().strip().startswith('.m emma'):
        await message.channel.send('https://tenor.com/bMe4t.gif')
 
+
     if message.content.lower().strip().startswith('.m wave'):
        await message.channel.send('https://tenor.com/blP4l.gif')
+
     
     if message.content.lower().strip().startswith('.m handshake'):
        await message.channel.send('https://tenor.com/blLXy.gif')
+
            
     if message.content.lower().strip().startswith('.m ick'):
        await message.channel.send('https://tenor.com/7w1S.gif') 
+
    
     if message.content.lower().strip().startswith('.m loser'):
        await message.channel.send('https://tenor.com/bgdt3.gif') 
+
+
     if message.content.lower().strip().startswith('.shocked'):
        await message.channel.send('https://media.giphy.com/media/3o7TKFq4jy3JeWyn8A/giphy.gif')
 
+
     if message.content.lower().strip().startswith('.wow'):
        await message.channel.send('https://media.giphy.com/media/1Zt3z4uEBPZQY/giphy.gif')
+
     
     if message.content.lower().strip().startswith('.m i hate you'):
        await message.channel.send('https://tenor.com/9eCN.gif') 
@@ -78,19 +90,25 @@ async def on_message(message):
     if message.content.lower().strip().startswith('.m sad'):
        await message.channel.send('https://tenor.com/btf9K.gif')   
 
+
     if message.content.lower().strip().startswith('.m lmao'):
        await message.channel.send('https://tenor.com/bh5zk.gif')    
+
 
     if message.content.lower().strip().startswith('.m ew'):
        await message.channel.send('https://tenor.com/4lKJ.gif')    
 
-    if message.content.lower().strip().startswith('.m cockroach calisthenics'):
-       await message.channel.send('https://youtu.be/KqNrAOrReNg')
 
     if message.content.lower().strip().startswith('.m roach'):
        await message.channel.send('https://tenor.com/992S.gif')  
 
+
     if message.content.lower().strip().startswith('.ian'):
        await message.channel.send('check out <@429723963157905410>\'s stream! https://twitch.tv/ianxian') 
+
+
+    if message.content.lower().strip().startswith('.ad'):
+       await message.channel.send('*The Men I Trust Discord* Our community is the largest supporting discord in regards to the Indie Band "Men I Trust." We have an extremely welcoming community, where **everyone** is welcome! Come check us out! We love you, A Lot. https://discord.gg/menitrust https://tinyurl.com/2fska59c') 
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
