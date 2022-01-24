dd4835c0e9999febc23aa82d9476df22dd2826e1
morgy222 <94820757+morgy222@users.noreply.github.com>
2022-01-24 03:31:08 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,7 +5,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
- await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Oncle Jazz"))
+ await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.listening, name="Oncle Jazz")) 
 
 @client.event
 async def on_message(message):
