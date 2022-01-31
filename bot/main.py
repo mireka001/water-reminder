cd3571ca692769d1734c4edbef06482aabe54877
m <94820757+morgy222@users.noreply.github.com>
2022-01-31 06:41:30 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -57,7 +57,7 @@ async def on_message(message):
     if message.content.lower().strip().startswith('.pandora'):
        await message.channel.send('https://www.pandora.com/station/play/110003941483222834') 
 
-    if message.content.lower().strip().startswith('Oncle commands'):
+    if message.content.lower().strip().startswith('.Doot'):
        await message.channel.send('Commands: .m emma /n/n.m proulx hairflip /n/n.m jammin /n/n.reddit /n/n.tumblr /n/n.youtube /n/n/.youtube music /n/n.spotify /n/n.apple music /n/n.iheartradio /n/n.pandora /n/n.ad /n/n.m message to meme /n/n.m meme /n/n.ian /n/n.benji') 
 
 
