af5aa14026a6b7a4dec8f01326fb308987589daa
m <94820757+morgy222@users.noreply.github.com>
2022-02-12 22:25:21 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -122,9 +122,9 @@ async def on_message(message):
        time.sleep(2)
        await message.delete()
 
-    elif message.content.lower().strip().startswith('.QOTD'):
+    elif message.content.lower().strip().startswith('.question'):
        await message.channel.send('"You\'re listening to Oncle Jazz"')
-       time.sleep(1)
+       time.sleep(2)
        await message.delete()
 
 
