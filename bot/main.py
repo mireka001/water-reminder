a812044698ebd29bc282b38f81a085f0dcb75b94
m <94820757+morgy222@users.noreply.github.com>
2022-04-10 21:23:25 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -18,16 +18,25 @@ async def on_message(message):
     #embed:
     if message.content.lower().strip().startswith('.dhinfo'):
        embed_m = discord.Embed(colour=0xfbf2b4)
-       embed_m.add_field(name="\n",
+       embed_m.add_field(name="-",
                          value='\n\nplease be kind <a:hartlemon:957115698327482428>\n\nwe love u here, no need to worry <a:hartlemon:957115698327482428>')
        embed_m.set_image(url="https://cdn.discordapp.com/attachments/957102779334270986/962568895812751410/outside_.png")                
        await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
+        
+    if message.content.lower().strip().startswith('.dhembed'):
+       embed_m = discord.Embed(colour=0xfbf2b4)
+       embed_m.add_field(name="-",
+                         value='<a:hartlemon:957115698327482428> <@&957540747538751519>\n\n<a:hartorange:957115698415542322> <@&957540790689755256>\n\n<a:hartlime:957115698352619610> <@&957540772398395392>\n\n<a:hartraspberry:957115698323259452> <@&957540812877611048>\n\n<:dhrknife:959494964943912990>  <@&957540922671923271>\n\n~\n\n<a:hartcherry:957115698231013407> <@&957542213905182760>\n\n<a:hartstrawberry:957115698491060255> <@&957542256456396820>')
+       embed_m.set_image(url="https://images-ext-1.discordapp.net/external/cltbshTV53E50-JHN9jct7H2cRFKWCwlzt0hhPlR6-w/https/media.discordapp.net/attachments/957479101298536458/957483402750230578/sunflower.jpg")                
+       await message.channel.send(embed=embed_m)
+       time.sleep(2)
+       await message.delete()
 
     #commands:
-    elif message.content.lower().strip().startswith('.dhembed'):
-       await message.channel.send('<a:hartlemon:957115698327482428> <@&957540747538751519>\n\n<a:hartorange:957115698415542322> <@&957540790689755256>\n\n<a:hartlime:957115698352619610> <@&957540772398395392>\n\n<a:hartraspberry:957115698323259452> <@&957540812877611048>\n\n<:dhrknife:959494964943912990>  <@&957540922671923271>\n\n~\n\n<a:hartcherry:957115698231013407> <@&957542213905182760>\n\n<a:hartstrawberry:957115698491060255> <@&957542256456396820>')
+    elif message.content.lower().strip().startswith('commandhere'):
+       await message.channel.send('bodytexthere')
        time.sleep(2)
        await message.delete()
 
