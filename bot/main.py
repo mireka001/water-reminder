e6f60c172b413a58774c8a9c51b8a0689dcb627a
m <94820757+bernache222@users.noreply.github.com>
2022-04-28 05:03:17 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -15,47 +15,61 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #embed:
-    if message.content.lower().strip().startswith('.dhinfo'):
-       embed_m = discord.Embed(colour=0xEA8D35)
-       embed_m.add_field(name="~",
-                         value='\n\n<:dhrduckheart:960366979657650176> please be kind\n\n<:dhrduckheart:960366979657650176> we love u here, no need to worry')
-       embed_m.set_image(url="https://media.discordapp.net/attachments/917513455395831818/962904025689387018/mr._duckie_-3.png")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-       await message.delete()
-        
-    if message.content.lower().strip().startswith('.dhembedrr1'):
-       embed_m = discord.Embed(colour=0x00000)
-       embed_m.add_field(name="~",
-                         value='\n\n<a:blackarrow:967236196818382939> <@&957540747538751519>\n\n<a:blackarrow:967236196818382939> <@&957540790689755256>\n\n<a:blackarrow:967236196818382939> <@&957540772398395392>\n\n<a:blackarrow:967236196818382939> <@&957540812877611048>\n\n<a:blackarrow:967236196818382939> <@&957540851901403166>\n\n<a:blackarrow:967236196818382939>  <@&957540922671923271>\n\n~\n\n<a:blackarrow:967236196818382939> <@&957542213905182760>\n\n<a:blackarrow:967236196818382939> <@&957542256456396820>')
-       embed_m.set_image(url="https://i.scdn.co/image/ab6761610000e5eba3a7cba23d68a4e73c3b8155")                
+#embed(s):
+
+
+    if message.content.lower().strip().startswith(';roles1'):
+       embed_m = discord.Embed(colour=0x000000)
+       embed_m.add_field(name="<3",
+                         value='<a:blackarrow:967236196818382939> <@&957540747538751519>\n\n<a:blackarrow:967236196818382939> <@&957540790689755256>\n\n<a:blackarrow:967236196818382939> <@&957540772398395392>\n\n<a:blackarrow:967236196818382939> <@&957540812877611048>\n\n<a:blackarrow:967236196818382939> <@&957540851901403166>\n\n<a:blackarrow:967236196818382939> <@&957542213905182760>\n\n<a:blackarrow:967236196818382939> <@&957542256456396820>')
+       embed_m.set_image(url="https://images-ext-1.discordapp.net/external/SqDgswiJyElYFa64YXiy7hXmbQghrJbs4SF5Ll4udOk/https/images-ext-2.discordapp.net/external/Mn4vIHHMLfOa-yPuL5gXZ6PgmW3nvHnTVJZJXLDqDaw/%253Fauto%253Dcompress%2526cs%253Dtinysrgb%2526dpr%253D1%2526w%253D500/https/images.pexels.com/photos/3048527/pexels-photo-3048527.png")                
        await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
 
-    if message.content.lower().strip().startswith('.dhembedrr2'):
-       embed_m = discord.Embed(colour=0xa40e26)
-       embed_m.add_field(name="~",
-                         value='\n\n<:dhoc:959584599841849344> <@&959587688086917233>\n\n<:dhasia:959584599728603187> <@&959577150393057290>\n\n<:dheu:959584599644700723> <@&959577017072881714>\n\n<:dhna:959584599229472839> <@&959576872717541387>\n\n<:dhsa:959584599355301988> <@&959576971455643708>')
-       embed_m.set_image(url="https://images-ext-2.discordapp.net/external/GnOPCL-Zjc2w94-Vi6JuXy1sd0ajHgYXQLnPAsFqrwk/https/media.discordapp.net/attachments/917513455395831818/962895880963362816/mr._duckie_-_2.png")                
+
+       
+    if message.content.lower().strip().startswith(';roles2'):
+       embed_m = discord.Embed(colour=0x000000)
+       embed_m.add_field(name="<3",
+                         value='<a:blackarrow:967236196818382939> <@&959577017072881714>\n\n<a:blackarrow:967236196818382939> <@&959587688086917233>\n\n<a:blackarrow:967236196818382939> <@&959576872717541387>\n\n<a:blackarrow:967236196818382939> <@&959576971455643708>\n\n<a:blackarrow:967236196818382939> <@&959577150393057290>')
+       embed_m.set_image(url="https://images-ext-2.discordapp.net/external/1zEYKNy-K9hDQsZpsosSOxGJRwIfzNJqjExHU0LQnfA/%3Fwidth%3D798%26height%3D1197/https/images-ext-1.discordapp.net/external/Qb_x2mHQhIhwP6JDWWKGhwLh2yGIUUPyrHOG_ihM2jw/%253Fixlib%253Drb-1.2.1%2526ixid%253DMnwxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBhbmQlMjB3aGl0ZSUyMHBob3RvZ3JhcGh5fGVufDB8fDB8fA%25253D%25253D%2526w%253D1000%2526q%253D80/https/images.unsplash.com/photo-1597871040916-4b4c20ba08dd")                
+       await message.channel.send(embed=embed_m)
+       time.sleep(2)
+
+
+
+       await message.delete()
+    if message.content.lower().strip().startswith(';roles3'):
+       embed_m = discord.Embed(colour=0x000000)
+       embed_m.add_field(name="<3",
+                         value='')
+       embed_m.set_image(url="")                
        await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
 
-    if message.content.lower().strip().startswith('.dhembedrr3'):
-       embed_m = discord.Embed(colour=0xEA8D35)
-       embed_m.add_field(name="~",
-                         value='\n\n<:dhrflower:959494929262968903> <@&963378319389261825>\n\n<:dhrflower:959494929262968903> <@&963378529536475176>\n\n<:dhrflower:959494929262968903>  <@&963378531994337360>\n\n<:dhrflower:959494929262968903> <@&963378533391024150>\n\n<:dhrflower:959494929262968903> <@&963378534364119080>\n\n<:dhrflower:959494929262968903> <@&963378535123275796>\n\n<:dhrflower:959494929262968903> <@&963378537304301568>\n\n<:dhrflower:959494929262968903> <@&963378537946026004>\n\n<:dhrflower:959494929262968903> <@&963378539246268417>\n\n<:dhrflower:959494929262968903> <@&963379237425926144>\n\n<:dhrflower:959494929262968903> <@&963388911848128562>\n\n<:dhrflower:959494929262968903> <@&963388936842010675>')
-       embed_m.set_image(url="https://cdn.discordapp.com/attachments/957102779334270986/963385766728323142/135-1356350_hue-y-duck-pixel-duck-hd-png-download-removebg-preview3.png")                
+
+
+    if message.content.lower().strip().startswith('blank'):
+       embed_m = discord.Embed(colour=0x000000)
+       embed_m.add_field(name="<3",
+                         value='')
+       embed_m.set_image(url="")                
        await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
 
-    #commands:
-    elif message.content.lower().strip().startswith('123'):
-       await message.channel.send('heyiworked')
+
+
+    if message.content.lower().strip().startswith('blank'):
+       embed_m = discord.Embed(colour=0x000000)
+       embed_m.add_field(name="<3",
+                         value='')
+       embed_m.set_image(url="")                
+       await message.channel.send(embed=embed_m)
        time.sleep(2)
        await message.delete()
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
