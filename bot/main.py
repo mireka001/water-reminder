409047ebcbfde4ad4c520f12dceffc0a8fd41648
m <94820757+morgy222@users.noreply.github.com>
2022-05-28 01:07:33 -0400

    change main

--- bot/main.py
+++ bot/main.py
@@ -18,51 +18,8 @@ async def on_message(message):
 #embed(s):
 
 
-    if message.content.lower().strip().startswith(';roles1'):
+    if message.content.lower().strip().startswith('command'):
        embed_m = discord.Embed(colour=0x000000)
-       embed_m.add_field(name="<3",
-                         value='<a:blackarrow:969162216307818556> <@&957540747538751519>\n\n<a:blackarrow:969162216307818556> <@&957540790689755256>\n\n<a:blackarrow:969162216307818556> <@&957540772398395392>\n\n<a:blackarrow:969162216307818556> <@&957540812877611048>\n\n<a:blackarrow:969162216307818556> <@&957540851901403166>\n\n<a:blackarrow:969162216307818556> <@&957542213905182760>\n\n<a:blackarrow:969162216307818556> <@&957542256456396820>')
-       embed_m.set_image(url="https://images-ext-1.discordapp.net/external/SqDgswiJyElYFa64YXiy7hXmbQghrJbs4SF5Ll4udOk/https/images-ext-2.discordapp.net/external/Mn4vIHHMLfOa-yPuL5gXZ6PgmW3nvHnTVJZJXLDqDaw/%253Fauto%253Dcompress%2526cs%253Dtinysrgb%2526dpr%253D1%2526w%253D500/https/images.pexels.com/photos/3048527/pexels-photo-3048527.png")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-       await message.delete()
-
-
-       
-    if message.content.lower().strip().startswith(';roles2'):
-       embed_m = discord.Embed(colour=0x000000)
-       embed_m.add_field(name="<3",
-                         value='<a:blackarrow:969162216307818556> <@&959577017072881714>\n\n<a:blackarrow:969162216307818556> <@&959587688086917233>\n\n<a:blackarrow:969162216307818556> <@&959576872717541387>\n\n<a:blackarrow:969162216307818556> <@&959576971455643708>\n\n<a:blackarrow:969162216307818556> <@&959577150393057290>')
-       embed_m.set_image(url="https://images-ext-2.discordapp.net/external/1zEYKNy-K9hDQsZpsosSOxGJRwIfzNJqjExHU0LQnfA/%3Fwidth%3D798%26height%3D1197/https/images-ext-1.discordapp.net/external/Qb_x2mHQhIhwP6JDWWKGhwLh2yGIUUPyrHOG_ihM2jw/%253Fixlib%253Drb-1.2.1%2526ixid%253DMnwxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBhbmQlMjB3aGl0ZSUyMHBob3RvZ3JhcGh5fGVufDB8fDB8fA%25253D%25253D%2526w%253D1000%2526q%253D80/https/images.unsplash.com/photo-1597871040916-4b4c20ba08dd")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-
-
-
-       await message.delete()
-    if message.content.lower().strip().startswith(';roles3'):
-       embed_m = discord.Embed(colour=0x000000)
-       embed_m.add_field(name="<3",
-                         value='<a:blackarrow:969162216307818556> <@&963378319389261825>\n\n<a:blackarrow:969162216307818556> <@&963378529536475176>\n\n<a:blackarrow:969162216307818556> <@&963378531994337360>\n\n<a:blackarrow:969162216307818556> <@&963378533391024150>\n\n<a:blackarrow:969162216307818556> <@&963378534364119080>\n\n<a:blackarrow:969162216307818556> <@&963378535123275796>\n\n<a:blackarrow:969162216307818556> <@&963378537304301568>\n\n<a:blackarrow:969162216307818556> <@&963378537946026004>\n\n<a:blackarrow:969162216307818556> <@&963378539246268417>\n\n<a:blackarrow:969162216307818556> <@&963379237425926144>\n\n<a:blackarrow:969162216307818556> <@&963388911848128562>\n\n<a:blackarrow:969162216307818556> <@&963388936842010675>')
-       embed_m.set_thumbnail(url="https://cdn.discordapp.com/emojis/963454587757559908.gif?size=240&quality=lossless")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-       await message.delete()
-
-
-    if message.content.lower().strip().startswith(';blank'):
-       embed_m = discord.Embed(colour=0x000000)
-       embed_m.add_field(name="<3",
-                         value='')
-       embed_m.set_image(url="")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-       await message.delete()
-
-
-
-    if message.content.lower().strip().startswith(';blank'):
-       embed_m = discord.Embed(colour=0x456098)
        embed_m.add_field(name="<3",
                          value='')
        embed_m.set_image(url="")                
