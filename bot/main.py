45bdb9930fb613a7f79d3b31ab5819bd5c26fdf8
m <94820757+bernache222@users.noreply.github.com>
2022-06-02 00:30:15 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,16 @@
 import discord
+from discord.ext import commands, tasks
 import os
 import time
 
+bot = commands.Bot('$')
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="my lovelies <3")) 
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="if you're drinking water")) 
 
+target_channel_id = 860564834553692162
 
 @client.event
 async def on_message(message):
@@ -16,15 +18,34 @@ async def on_message(message):
         return
 
 #embed(s):
+@tasks.loop(hours=3)
+async def called_every_three_hours():
+    message_channel = bot.get_channel(target_channel_id)
+    print(f"Got channel {message_channel}")
+    await message_channel.send("Tomen agua porfa <3")
+
+#Cron task reminder every 3 hours loop console print
+@called_every_three_hours.before_loop
+async def before():
+    await bot.wait_until_ready()
+    print("Finished waiting")
+
+called_every_three_hours.start()
 
 
-    if message.content.lower().strip().startswith('command'):
-       embed_m = discord.Embed(colour=0x000000)
-       embed_m.add_field(name="<3",
-                         value='')
-       embed_m.set_image(url="")                
-       await message.channel.send(embed=embed_m)
-       time.sleep(2)
-       await message.delete()
+#Funcion para enviar mensaje con el comando
+
+@client.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.lower().strip().startswith('$agua'):
+        await message.channel.send('Tomen aguita cabros <3')
+
+
+    if message.content.lower().strip().startswith('$benji'):
+        await message.channel.send('i FINALLY got the CLI to work')
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
